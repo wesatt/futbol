d810456e90ba1a2d6b342f8ff0aa130c8c8d5ec7
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-16 22:07:31 -0700

    Add method for winningest coach

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require "csv"
 require_relative "./game_teams"
 require_relative "./games"
@@ -24,10 +25,51 @@ class StatTracker
     # stat_tracker.games = Game.create_list_of_game(stat_tracker.games_csv)
   end
 
-
-    def team_info(team_id)
-      teams.by_id(team_id)
+  def winningest_coach(season_arg)
+    coach_by_season = Hash.new(0)
+    team_id_for_season = Hash.new(0)
+    @teams.team_id.each do |team|
+      team_id_for_season[team] = {win: 0, total: 0}
     end
+
+    # adds wins and totals
+    @games.season.each_with_index do |season, index|
+      if season == season_arg
+        if @games.away_goals[index].to_i > @games.home_goals[index].to_i
+          team_id_for_season[@games.away_team_id[index]][:win] += 1
+        elsif @games.away_goals[index].to_i < @games.home_goals[index].to_i
+          team_id_for_season[@games.home_team_id[index]][:win] += 1
+        end
+        team_id_for_season[@games.away_team_id[index]][:total] += 1
+        team_id_for_season[@games.home_team_id[index]][:total] += 1
+      end
+    end
+
+    # adds coach
+    team_id_for_season.each do |key_id, value_hash|
+      @game_teams.head_coach.each_with_index do |coach, index|
+        team_id_for_season[@game_teams.team_id[index]][:coach] = coach
+      end
+    end
+
+    # makes hash of coaches and wins
+    team_id_for_season.each do |key_id, value_hash|
+      percentage = (value_hash[:win].to_f / value_hash[:total].to_f).round(2)
+      coach_by_season[value_hash[:coach]] = if percentage > 0
+        percentage
+      else
+        0
+      end
+    end
+
+    best_coach = coach_by_season.max_by { |k, v| v }
+    best_coach[0]
+  end
+
+
+  def team_info(team_id)
+    teams.by_id(team_id)
+  end
 
   # Start Game Statistics methods
   def highest_total_score
@@ -62,4 +104,9 @@ class StatTracker
     @games.games_by_season_hash[:average_goals]
   end
   # End Game Statistics methods
+  # Start Season Statistics methods
+
+
+
+  # End Season Statistics methods
 end
