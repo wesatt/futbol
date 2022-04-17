3a386413a4b966ec0f001fbfd9ff20f1032bd3c6
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-16 22:09:35 -0700

    Add test for worst coach

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -89,5 +89,9 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.winningest_coach('20142015')).to eq ("Alain Vigneault")
   end
 
+  it 'has worst coach' do
+    expect(@stat_tracker.worst_coach('20132014')).to eq ("Peter DeBoer")
+    expect(@stat_tracker.worst_coach('20142015')).to eq ("Craig MacTavish").or(eq("Ted Nolan"))
+  end
 
 end
